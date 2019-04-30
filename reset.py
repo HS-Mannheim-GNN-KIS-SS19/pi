e2d525b4474c3d15411a719a3a467438e0a17040
Your Name <you@example.com>
2019-04-30 11:18:12 +0200

    adjusted reset angles

--- reset.py
+++ reset.py
@@ -10,6 +10,6 @@ _CLUTCH_CHANNEL = 3
 _kit = adafruit_servokit.ServoKit(channels=8)
 
 _kit.servo[_BASE_CHANNEL].angle = 0
-_kit.servo[_ARM_VERTICAL_CHANNEL].angle = 0
+_kit.servo[_ARM_VERTICAL_CHANNEL].angle = 75
 _kit.servo[_ARM_HORIZONTAL_CHANNEL].angle = 0
 _kit.servo[_CLUTCH_CHANNEL].angle = 45
