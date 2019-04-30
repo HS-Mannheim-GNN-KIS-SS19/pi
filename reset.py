491d39a573e38375418a54fbcd80c48e8c3ec53f
Markus Schudt <1724070@stud.hs-mannheim.de>
2019-04-30 11:13:17 +0200

    ready for testing2

---
+++ reset.py
@@ -0,0 +1,15 @@
+import adafruit_servokit
+
+_BASE_CHANNEL = 0
+_ARM_VERTICAL_CHANNEL = 1
+_ARM_HORIZONTAL_CHANNEL = 2
+_CLUTCH_CHANNEL = 3
+
+
+
+_kit = adafruit_servokit.ServoKit(channels=8)
+
+_kit.servo[_BASE_CHANNEL].angle = 0
+_kit.servo[_ARM_VERTICAL_CHANNEL].angle = 0
+_kit.servo[_ARM_HORIZONTAL_CHANNEL].angle = 0
+_kit.servo[_CLUTCH_CHANNEL].angle = 45
