d2a49ed055bb300a38b58c52a7267f0396bac93c
Phoenix <34738755+GerPhoenix@users.noreply.github.com>
2019-05-01 20:48:43 +0200

    [eezybot_test] Test designed to adjust the min and max Rotations that the Servos can perform

--- eezybot_test.py
+++ eezybot_test.py
@@ -1,28 +1,31 @@
-import sys
-import time
-
-import eezybot
-
-
-def servo_test():
-    eezybot.reset()
-    time.sleep(3)
-    eezybot.base.rotate_and_wait(180)
-    time.sleep(1)
-    eezybot.armVertical.rotate_and_wait(180)
-    time.sleep(1)
-    eezybot.armHorizontal.rotate_and_wait(180)
-    time.sleep(1)
-    eezybot.clutch.grab()
-    time.sleep(1)
-    eezybot.reset()
-    time.sleep(3)
+from time import sleep
+from eezybotServoController import eezybot
+from constants import *
 
 
-try:
-    servo_test()
-finally:
-    sys.exit(0)
+def servo_test_max():
+    eezybot.base.rotate(BASE.MAX).wait()
+    eezybot.verticalArm.rotate(VERTICAL.MAX).wait()
+    eezybot.horizontalArm.rotate(HORIZONTAL.MAX).wait()
+    eezybot.clutch.rotate(CLUTCH.MAX).wait()
+
+
+def servo_test_min():
+    eezybot.base.rotate(BASE.MIN).wait()
+    eezybot.verticalArm.rotate(VERTICAL.MIN).wait()
+    eezybot.horizontalArm.rotate(HORIZONTAL.MIN).wait()
+    eezybot.clutch.rotate(CLUTCH.MIN).wait()
+
+
+def testBoth():
+    eezybot.to_default().wait_for_all()
+    servo_test_max()
+    eezybot.wait_for_all()
+    servo_test_min()
+
+
+eezybot.start().print_performed_rotations(True).activate_key_listener()
+testBoth()
 
 # shoud rotate the base 90 degree
 # eezybot.base.rotate_to(0, 1)
